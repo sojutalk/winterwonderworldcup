27866d38d7a582b23c2b06cdfe35b836a3a31e53
sojutalk <95378491+sojutalk@users.noreply.github.com>
2021-12-01 16:10:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(function(){
-      alert("ver3");
+      alert("ver4");
 });
 
 $(document).keydown(function(e){ 
@@ -15,36 +15,6 @@ $(document).bind("contextmenu",function(e) {
 	e.preventDefault(); 
  
 }); 
-
-var currentHtmlContent; 
- 
-var element = new Image(); 
- 
-var elementWithHiddenContent = document.querySelector("#element-to-hide"); 
- 
-var innerHtml = elementWithHiddenContent.innerHTML; 
- 
- 
- 
-element.__defineGetter__("id", function() { 
- 
-    currentHtmlContent= ""; 
- 
-}); 
- 
- 
- 
-setInterval(function() { 
- 
-    currentHtmlContent= innerHtml; 
- 
-    console.log(element); 
- 
-    console.clear();  
- 
-    elementWithHiddenContent.innerHTML = currentHtmlContent; 
- 
-}, 1000); 
 
 $(document).ready(function () {
 
